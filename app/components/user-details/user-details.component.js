1ec918f90f60f8f4c2bc332f8eb6d5678f493f36
Oleg <oleg.sokolansky@chainbalance.com>
2016-11-07 17:32:43 +0200

    20161107 work

--- app/components/user-details/user-details.component.js
+++ app/components/user-details/user-details.component.js
@@ -2,9 +2,13 @@
 angular.module('smartSupply.userDetails')
     .component('userDetails',{
         templateUrl: "components/user-details/user-details.template.html",
-        bindings: { user: "<", onCreate: "&", onUpdate: "&"},
+        bindings: {
+            user: "<",
+            onCreate: "&",
+            onUpdate: "&"},
         controller: [function () {
             var ctrl = this;
+            //ctrl.userForView = user;
             ctrl.create = function (user) {
                 ctrl.onCreate({user: ctrl.user});
             };
