eda66c22a5ffee96fab4d0dd1c363d1a4798e552
Oleg <oleg.sokolansky@chainbalance.com>
2016-11-22 17:33:20 +0200

    20161122 work

--- app/components/password-confirmation/password-confirmation.directive.js
+++ app/components/password-confirmation/password-confirmation.directive.js
@@ -1,3 +1,31 @@
 /**
  * Created by oleg on 11/21/2016.
  */
+angular.module('smartSupply.passwordConfirmation')
+    .directive('passwordconfirmation', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl){
+                ctrl.$validators.passwordconfirmation = function (modelValue, viewValue) {
+                    if (!attrs.passwordconfirmation) {
+                        console.error('passwordconfirmation expects a model as an argument');
+                        return;
+                    }
+                    scope.$watch(attrs.passwordconfirmation, function (value) {
+                        if (model.$viewValue !== undefined && model.$viewValue !== '') {
+                            model.$setValidity('passwordconfirmation', value === model.$viewValue);
+                        }
+                    });
+                    model.$parsers.push(function (value) {
+                        if (value === undefined || value === '') {
+                            model.$setValidity('passwordconfirmation', true);
+                            return value;
+                        }
+                        var isValid = value === scope.$eval(attrs.passwordconfirmation);
+                        model.$setValidity('passwordconfirmation', isValid);
+                        return isValid ? value : undefined;
+                    });
+                };
+            }
+        };
+    });
--- app/components/password-confirmation/password-confirmation.directive.spec.js
+++ app/components/password-confirmation/password-confirmation.directive.spec.js
@@ -2,14 +2,26 @@
  * Created by oleg on 11/21/2016.
  */
 'use strict';
-angular.module('smartSupply.passwordConfirmation')
-.directive('passwordconfirmation', function() {
-    return {
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl){
-            ctrl.$validators.passwordconfirmation = function (modelValue, viewValue) {
-                //if ()
-            };
-        }
-    };
+describe('password-confirmation', function () {
+    beforeEach(module('ngModel', 'smartSupply.passwordConfirmation'));
+    var $rootScope, $compile, elm;
+    beforeEach(inject(function(_$rootScope_, _$compile_){
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+    describe('validation', function() {
+        beforeEach(function () {
+            elm = angular.element(
+                '<form name="form">' +
+                    '<input type="password" ng-model="login.password">' +
+                    '<input type="password" ng-model="login.verify" passwordconfirmation="login.password" name="verify">' +
+                    '<span ng-show="form.verify.$error.passwordconfirmation">Must be equal</span>' +
+                '</form>');
+            $compile(elm)($rootScope);
+            $rootScope.$diggest();
+        });
+        it('check', function () {
+            console.log(elm);
+        });
+    });
 });
